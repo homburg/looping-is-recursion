e09f4761d73b550dd1645af4d8b627725103d441
Thomas B Homburg <thomas@homburg.dk>
2014-03-25 21:34:00 +0100

    Add solutions

--- src/looping_is_recursion.clj
+++ src/looping_is_recursion.clj
@@ -1,13 +1,26 @@
 (ns looping-is-recursion)
 
 (defn power [base exp]
-  ":(")
+  (letfn [(helper [exp acc]
+                  (cond
+                   (zero? exp) 1
+                   (= 1 exp) acc
+                   :else (recur (dec exp) (* base acc))))]
+    (helper exp base)))
 
 (defn last-element [a-seq]
-  ":(")
+  (letfn [(helper [a-seq acc]
+                  (cond
+                   (empty? a-seq) acc
+                   :else (recur (rest a-seq) (first a-seq))))]
+    (helper a-seq nil)))
 
 (defn seq= [seq1 seq2]
-  ":(")
+  (or (and (empty? seq1) (empty? seq2))
+      (and
+       (= (count seq1) (count seq2))
+       (= (first seq1) (first seq2))
+       (recur (rest seq1) (rest seq2)))))
 
 (defn find-first-index [pred a-seq]
   ":(")
